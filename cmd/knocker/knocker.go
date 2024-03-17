719cd48564e159a66a69483dcefeacc839841e09
Jérôme MOUREY <jerome@everycheck.fr>
2024-03-18 00:52:26 +0100

    extract pcap code from cmd and move it into a pkg

--- cmd/knocker/knocker.go
+++ cmd/knocker/knocker.go
@@ -2,45 +2,19 @@ package main
 
 import (
 	"app/knocker"
+	"app/pcap"
 	"fmt"
-	"strconv"
 	"time"
-
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pcap"
 )
 
 const Action = "knocker"
 
 func Run(args []string) error {
-	if len(args) > 5 {
-		fmt.Errorf("Usage: app knocker <interface> <port1> <port2> <port3> <port4>")
+	if len(args) < 1 {
+		return fmt.Errorf("Usage: app knocker <interface>")
 	}
-	iface := args[1]
-	ports := make([]uint16, 4)
-	for i := 0; i < 4; i++ {
-		port, err := strconv.Atoi(args[i+1])
-		if err != nil || port < 1 || port > 65535 {
-			return fmt.Errorf("Invalid port number: %s", args[i+1])
-		}
-		ports[i] = uint16(port)
-	}
+	iface := args[0]
 
-	// Open device
-	handle, err := pcap.OpenLive(iface, 1600, true, pcap.BlockForever)
-	if err != nil {
-		return fmt.Errorf("cannot open '%s' : %w", iface, err)
-	}
-	defer handle.Close()
-
-	var filter = "udp"
-	err = handle.SetBPFFilter(filter)
-	if err != nil {
-		return fmt.Errorf("cannot filter with 'udp': %w", err)
-	}
-	fmt.Printf("Monitoring for port knocking sequence on %s: %v\n", iface, ports)
-	// Initialize the sequence tracker with the provided ports
 	tracker := knocker.NewUserKnocker(knocker.UserKnockerConfig{
 		Users: map[string]string{
 			"admin": "password",
@@ -53,23 +27,11 @@ func Run(args []string) error {
 		MinInterval:              time.Second,
 	})
 
-	// Use the handle as a packet source to process all packets
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	packetSource.DecodeOptions.Lazy = true
-	packetSource.DecodeOptions.NoCopy = true
-
-	for packet := range packetSource.Packets() {
-		// Process packet here
-		if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
-			udp, _ := udpLayer.(*layers.UDP)
-			srcIP := packet.NetworkLayer().NetworkFlow().Src().String()
-			dstPort := udp.DstPort
-
-			// Check if the current packet is part of the sequence
-			if user, ok := tracker.CheckSequence(srcIP, uint16(dstPort), time.Now()); ok {
-				fmt.Printf("%s has completed the sequence from %s \n", user, srcIP)
-			}
+	return pcap.Run(iface, func(ip string, port uint16) {
+		user, ok := tracker.CheckSequence(ip, port, time.Now())
+		if ok {
+			fmt.Println(user, "has validated the challenge")
 		}
-	}
-	return nil
+	})
+
 }
---
+++ pcap/pcap.go
@@ -0,0 +1,63 @@
+package pcap
+
+import (
+	"fmt"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+type ProcessPacket func(ip string, port uint16)
+
+func Run(iface string, processPacket ProcessPacket) error {
+
+	handle, err := pcap.OpenLive(iface, 1600, true, pcap.BlockForever)
+	if err != nil {
+		return fmt.Errorf("cannot open '%s' : %w", iface, err)
+	}
+	defer handle.Close()
+
+	// Set filter
+	var filter = "udp"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		return fmt.Errorf("cannot filter with 'udp': %w", err)
+	}
+	fmt.Printf("Capturing UDP packets on %s\n", iface)
+
+	// Use the handle as a packet source to process all packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	packetSource.DecodeOptions.Lazy = true
+	packetSource.DecodeOptions.NoCopy = true
+
+	for packet := range packetSource.Packets() {
+		// Process packet here
+		// fmt.Println("Got a UDP packet:")
+		ip, port := extractPacketInfo(packet)
+		processPacket(ip, port)
+	}
+	return nil
+}
+
+func extractPacketInfo(packet gopacket.Packet) (string, uint16) {
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer == nil {
+		return "", 0
+	}
+	ip, ok := ipLayer.(*layers.IPv4)
+	if !ok {
+		return "", 0
+	}
+
+	udpLayer := packet.Layer(layers.LayerTypeUDP)
+	if udpLayer == nil {
+		return ip.SrcIP.String(), 0
+	}
+	udp, ok := udpLayer.(*layers.UDP)
+	if !ok {
+		return ip.SrcIP.String(), 0
+	}
+
+	return ip.SrcIP.String(), uint16(udp.DstPort)
+}
